1696173860d4a203231f048673d7ef4c81aba16a
s14006 <s14006@std.it-college.ac.jp>
2015-06-03 11:55:10 +0900

    List 2-1

---
+++ Adapter/src/Banner.java
@@ -0,0 +1,19 @@
+/**
+ * Created by kabotya on 15/06/03.
+ */
+public class Banner {
+
+    private String string;
+
+    public Banner(String string) {
+        this.string = string;
+    }
+
+    public void showWithParen() {
+        System.out.println("(" + string + ")");
+    }
+
+    public void showWithAster() {
+        System.out.println("*" + string + "*");
+    }
+}
