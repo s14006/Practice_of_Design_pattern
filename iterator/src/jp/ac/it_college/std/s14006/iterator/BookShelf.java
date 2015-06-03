7eb1939370a26b22a758d04ac90b655a45bf650a
s14006 <s14006@std.it-college.ac.jp>
2015-06-03 10:44:55 +0900

    List 1-4

---
+++ iterator/src/jp/ac/it_college/std/s14006/iterator/BookShelf.java
@@ -0,0 +1,31 @@
+package jp.ac.it_college.std.s14006.iterator;
+
+/**
+ * Created by kabotya on 15/06/03.
+ */
+public class BookShelf {
+
+    private Book[] books;
+    private int last = 0;
+
+    public BookShelf(int maxsize) {
+        this.books = new Book[maxsize];
+    }
+
+    public Book getBookAt(int index) {
+        return books[index];
+    }
+
+    public void appendBook(Book book) {
+        this.books[last] = book;
+        last++;
+    }
+
+    public int getlength() {
+        return last;
+    }
+
+    public Iterator iterator() {
+        return new BookShelfIterator(this);
+    }
+}
