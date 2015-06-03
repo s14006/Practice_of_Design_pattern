d764e51c9f98ecdfb5e433bb492faf1c089cb13a
s14006 <s14006@std.it-college.ac.jp>
2015-06-03 13:11:26 +0900

    List 2-5

---
+++ adapter/src/jp/ac/it_college/std/s14006/adapter/Print2.java
@@ -0,0 +1,10 @@
+package jp.ac.it_college.std.s14006.adapter;
+
+/**
+ * Created by kabotya on 15/06/03.
+ */
+public abstract class Print2 {
+
+    public abstract void printWeak();
+    public abstract void printStrong();
+}
