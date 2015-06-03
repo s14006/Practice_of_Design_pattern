41b98df6e857ed4cdeb606fde0d89c3fce360118
s14006 <s14006@std.it-college.ac.jp>
2015-06-03 12:00:49 +0900

    List 2-3

---
+++ Adapter/src/PrintBanner.java
@@ -0,0 +1,19 @@
+/**
+ * Created by kabotya on 15/06/03.
+ */
+public class PrintBanner extends Banner implements Print{
+
+    public PrintBanner(String string) {
+        super(string);
+    }
+
+    @Override
+    public void printWeak() {
+        showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+        showWithAster();
+    }
+}
