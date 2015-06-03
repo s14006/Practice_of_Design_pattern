afdcd015f2eb66d5d08dfdfefd1455a750e0144d
s14006 <s14006@std.it-college.ac.jp>
2015-06-03 10:31:49 +0900

    List 1-2

---
+++ iterator/src/jp/ac/it_college/std/s14006/iterator/Aggregate.java
@@ -0,0 +1,9 @@
+package jp.ac.it_college.std.s14006.iterator;
+
+/**
+ * Created by kabotya on 15/06/03.
+ */
+public interface Aggregate {
+
+    public abstract Iterator iterator();
+}
---
+++ iterator/src/jp/ac/it_college/std/s14006/iterator/Iterator.java
@@ -0,0 +1,10 @@
+package jp.ac.it_college.std.s14006.iterator;
+
+/**
+ * Created by kabotya on 15/06/03.
+ */
+public interface Iterator {
+
+    public abstract boolean hasNext();
+    public abstract Object next();
+}
