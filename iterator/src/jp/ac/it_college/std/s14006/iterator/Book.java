c2c62a0ded667c0a4925048f59cbcd3cca69c568
s14006 <s14006@std.it-college.ac.jp>
2015-06-03 10:38:22 +0900

    List 1-3

---
+++ iterator/src/jp/ac/it_college/std/s14006/iterator/Book.java
@@ -0,0 +1,17 @@
+package jp.ac.it_college.std.s14006.iterator;
+
+/**
+ * Created by kabotya on 15/06/03.
+ */
+public class Book {
+
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
