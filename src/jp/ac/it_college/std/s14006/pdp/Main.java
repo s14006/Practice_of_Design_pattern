a6c216ad16348e1afd98333c83439aab6d8e28ab
s14006 <s14006@std.it-college.ac.jp>
2015-06-03 11:40:47 +0900

    List 1-6

---
+++ src/jp/ac/it_college/std/s14006/pdp/Main.java
@@ -0,0 +1,26 @@
+package jp.ac.it_college.std.s14006.pdp;
+
+import jp.ac.it_college.std.s14006.iterator.Book;
+import jp.ac.it_college.std.s14006.iterator.BookShelf;
+import jp.ac.it_college.std.s14006.iterator.Iterator;
+
+/**
+ * Created by kabotya on 15/06/03.
+ */
+public class Main {
+    public static void main(String[] ages) {
+
+        BookShelf bookShelf = new BookShelf(4);
+        bookShelf.appendBook(new Book("Around the World in 80 Days"));
+        bookShelf.appendBook(new Book("Bible"));
+        bookShelf.appendBook(new Book("Cinderella"));
+        bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+
+        Iterator it = bookShelf.iterator();
+
+        while (it.hasNext()) {
+            Book book = (Book) it.next();
+            System.out.println(book.getName());
+        }
+    }
+}
