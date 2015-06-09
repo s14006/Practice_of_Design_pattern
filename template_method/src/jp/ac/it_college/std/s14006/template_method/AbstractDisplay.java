4d9baa7c2673d9cc2714cb142cda47e3bd2df4e7
s14006 <s14006@std.it-college.ac.jp>
2015-06-09 10:00:33 +0900

    List 3-1

---
+++ template_method/src/jp/ac/it_college/std/s14006/template_method/AbstractDisplay.java
@@ -0,0 +1,22 @@
+package jp.ac.it_college.std.s14006.template_method;
+
+/**
+ * Created by kabotya on 15/06/09.
+ */
+public class AbstractDisplay {
+
+    public abstract void open();
+    public abstract void print();
+    public abstract void close();
+
+    public final void display() {
+
+        open();
+
+        for (int i = 0; i < 5; i++) {
+            print();
+        }
+
+        close();
+    }
+}
