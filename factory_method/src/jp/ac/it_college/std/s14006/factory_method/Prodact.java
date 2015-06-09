2d2a085c790ae7277107191e473ffe07dc16a9a2
s14006 <s14006@std.it-college.ac.jp>
2015-06-09 11:32:57 +0900

    List 4-1

---
+++ factory_method/src/jp/ac/it_college/std/s14006/factory_method/Prodact.java
@@ -0,0 +1,10 @@
+package jp.ac.it_college.std.s14006.factory_method;
+package framework;
+
+/**
+ * Created by kabotya on 15/06/09.
+ */
+public abstract class Prodact {
+
+    public abstract void use();
+}
