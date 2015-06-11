30174344d53f787012adc54dc21b146e4a3eaec2
s14006 <s14006@std.it-college.ac.jp>
2015-06-11 11:00:46 +0900

    List 8-1

---
+++ abstract_factory/src/jp/ac/it_college/std/s14006/abstract_factory/Item.java
@@ -0,0 +1,15 @@
+package jp.ac.it_college.std.s14006.abstract_factory;
+
+/**
+ * Created by kabotya on 15/06/11.
+ */
+public abstract class Item {
+
+    protected String caption;
+
+    public Item(String caption) {
+        this.caption = caption;
+    }
+
+    public abstract String makeHTML();
+}
