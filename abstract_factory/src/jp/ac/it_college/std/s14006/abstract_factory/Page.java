6ab6113a2ebda3d5ad848aaad27203a74904a86e
s14006 <s14006@std.it-college.ac.jp>
2015-06-11 11:18:33 +0900

    List 8-4

---
+++ abstract_factory/src/jp/ac/it_college/std/s14006/abstract_factory/Page.java
@@ -0,0 +1,36 @@
+package jp.ac.it_college.std.s14006.abstract_factory;
+
+import java.io.*;
+import java.util.ArrayList;
+/**
+ * Created by kabotya on 15/06/11.
+ */
+public abstract class Page {
+
+    protected String title;
+    protected String author;
+    protected ArrayList content = new ArrayList();
+
+    public Page(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public void add(Item item) {
+        content.add(item);
+    }
+
+    public void output() {
+        try {
+            String filename = title + ".html";
+            Writer writer = new FileWriter(filename);
+            writer.write(this.makeHTML());
+            writer.close();
+            System.out.println(filename + "を作成しました");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public abstract String makeHTML();
+}
