0b46d0aaf29ab998ac4341af1f363da81dbccf8e
s14006 <s14006@std.it-college.ac.jp>
2015-06-17 13:30:27 +0900

    List 12-1

---
+++ decorator/src/jp/ac/it_college/std/s14006/decorator/Display.java
@@ -0,0 +1,17 @@
+package jp.ac.it_college.std.s14006.decorator;
+
+/**
+ * Created by kabotya on 15/06/17.
+ */
+public abstract class Display {
+
+    public abstract int getColumns;
+    public abstract int getRows();
+    public abstract String getRowText(int row);
+
+    public final void show() {
+        for (int i = 0; i < getRows(); i++) {
+            System.out.println(getRowText(i));
+        }
+    }
+}
