5fe6643dcfa24f906ab547f309ebe94e41702589
s14006 <s14006@std.it-college.ac.jp>
2015-06-17 11:43:00 +0900

    List 11-1

---
+++ composite/src/jp/ac/it_college/std/s14006/composite/Entry.java
@@ -0,0 +1,24 @@
+package jp.ac.it_college.std.s14006.composite;
+
+/**
+ * Created by kabotya on 15/06/17.
+ */
+public abstract class Entry {
+
+    public abstract String getName();
+    public abstract int getSize();
+
+    public Entry add(Entry entry) throws FileTreatmentException {
+        throw new FileTeatmentException();
+    }
+
+    public void printList() {
+        printList("");
+    }
+
+    protected abstract void printList(String prefix);
+
+    public String toString() {
+        return getName() + " (" + getSize() + ") ";
+    }
+}
