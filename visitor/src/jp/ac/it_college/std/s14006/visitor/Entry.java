fd76ff7b421a61e89b3177b511a79f09f0f455f0
s14006 <s14006@std.it-college.ac.jp>
2015-06-17 14:36:20 +0900

    List 13-3

---
+++ visitor/src/jp/ac/it_college/std/s14006/visitor/Entry.java
@@ -0,0 +1,25 @@
+package jp.ac.it_college.std.s14006.visitor;
+
+import java.util.Iterator;
+
+/**
+ * Created by kabotya on 15/06/17.
+ */
+public abstract class Entry implements Element{
+
+    public abstract String getName();
+    public abstract int getSize();
+
+    public Entry add(Entry entry) throws FileTreatmentException {
+        throw new FileTreatmentException();
+    }
+
+    public Iterator iterator() throws FileTreatmentException {
+        throw new FileTreatmentException();
+    }
+
+    public String toString() {
+        return getName() + " (" + getSize() + ") ";
+    }
+
+}
