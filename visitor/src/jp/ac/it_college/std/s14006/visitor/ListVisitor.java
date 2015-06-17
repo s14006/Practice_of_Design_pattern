c24f15cfb5194306735a88fe11e34cf45dfd6812
s14006 <s14006@std.it-college.ac.jp>
2015-06-17 14:56:11 +0900

    List 13-6

---
+++ visitor/src/jp/ac/it_college/std/s14006/visitor/ListVisitor.java
@@ -0,0 +1,30 @@
+package jp.ac.it_college.std.s14006.visitor;
+
+import java.util.Iterator;
+
+/**
+ * Created by kabotya on 15/06/17.
+ */
+public class ListVisitor extends Visiter {
+
+    private String currentdir = "";
+
+    public void visit(File file) {
+        System.out.println(currentdir + "/" + file);
+    }
+
+    public void visit(Directory directory) {
+        System.out.println(currentdir + "/" + directory.getName());
+
+        String savedir = currentdir;
+        currentdir = currentdir + "/" + directory.getName();
+        Iterator it = directory.iterator();
+
+        while (it.hasNext()) {
+            Entry entry = (Entry)it.next();
+            entry.accept(this);
+        }
+
+        currentdir = savedir;
+    }
+}
