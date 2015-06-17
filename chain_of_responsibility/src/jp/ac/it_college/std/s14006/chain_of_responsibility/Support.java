770e97528f84af0d17c7d97c1ed90b655f1d9641
s14006 <s14006@std.it-college.ac.jp>
2015-06-17 15:48:19 +0900

    List 14-2

--- chain_of_responsibility/src/jp/ac/it_college/std/s14006/chain_of_responsibility/Support.java
+++ chain_of_responsibility/src/jp/ac/it_college/std/s14006/chain_of_responsibility/Support.java
@@ -16,4 +16,28 @@ public abstract class Support {
         this.next = next;
         return next;
     }
+
+    public final void support(Trouble trouble) {
+        if (resolve(trouble)) {
+            done(trouble);
+        } else if (next != null) {
+            next.support(trouble);
+        } else {
+            fail(trouble);
+        }
+    }
+
+    public String toString() {
+        return "[" + name + "]";
+    }
+
+    protected abstract boolean resolve(Trouble trouble);
+
+    protected void done(Trouble trouble) {
+        System.out.println(trouble + " is resolved by " + this + ".");
+    }
+
+    protected void fail(Trouble trouble) {
+        System.out.println(trouble + " cannot be resolved.");
+    }
 }
