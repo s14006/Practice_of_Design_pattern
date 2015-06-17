faf4370f27478caa9c6f14d45ca537cbcf6e638a
s14006 <s14006@std.it-college.ac.jp>
2015-06-17 11:15:51 +0900

    List 10-6

--- src/jp/ac/it_college/std/s14006/pdp/Main.java
+++ src/jp/ac/it_college/std/s14006/pdp/Main.java
@@ -26,6 +26,10 @@ import jp.ac.it_college.std.s14006.prototype.framework.Manager;
 import jp.ac.it_college.std.s14006.prototype.framework.MessageBox;
 import jp.ac.it_college.std.s14006.prototype.framework.Product;
 import jp.ac.it_college.std.s14006.singleton.Singleton;
+import jp.ac.it_college.std.s14006.strategy.Hand;
+import jp.ac.it_college.std.s14006.strategy.Player;
+import jp.ac.it_college.std.s14006.strategy.ProbStrategy;
+import jp.ac.it_college.std.s14006.strategy.WinningStrategy;
 import jp.ac.it_college.std.s14006.template_method.AbstractDisplay;
 import jp.ac.it_college.std.s14006.template_method.CharDisplay;
 import jp.ac.it_college.std.s14006.template_method.StringDisplay;
@@ -36,7 +40,7 @@ import jp.ac.it_college.std.s14006.template_method.StringDisplay;
 public class Main {
     public static void main(String[] args) {
 
-        Chapter9();
+        Chapter10(new String[] {"134", "15"});
 
     }
 
@@ -216,6 +220,47 @@ public class Main {
         d3.dispaly();
         d3.multiDisplay(5);
     }
+
+    public static void Chapter10(String[] args) {
+
+        if (args.length != 2) {
+            System.out.println("Usage: java Main randomseed1 randomseed2");
+            System.out.println("Example: java Main 314 15");
+            System.exit(0);
+        }
+
+        int seed1 = Integer.parseInt(args[0]);
+        int seed2 = Integer.parseInt(args[1]);
+
+        Player player1 = new Player("Taro", new WinningStrategy(seed1));
+        Player player2 = new Player("Hana", new ProbStrategy(seed2));
+
+        for (int i = 0; i < 10000; i++) {
+            Hand nextHand1 = player1.nextHand();
+            Hand nextHand2 = player2.nextHand();
+
+            if (nextHand1.isStrongerThan(nextHand2)) {
+                System.out.println("Winner: " + player1);
+                player1.win();
+                player2.lose();
+
+            } else if (nextHand2.isStrongerThan(nextHand1)) {
+                System.out.println("Winner: " + player2);
+                player1.lose();
+                player2.win();
+
+            } else {
+                System.out.println("Even…");
+                player1.even();
+                player2.even();
+            }
+        }
+
+        System.out.println("Total result:");
+        System.out.println(player1.toString());
+        System.out.println(player2.toString());
+    }
+
 
 
     //配列のやつ
