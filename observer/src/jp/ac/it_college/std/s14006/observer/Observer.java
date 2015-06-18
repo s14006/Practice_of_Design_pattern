a601edfdae2e251cb32ecbb7ebb4b93d9881f702
s14006 <s14006@std.it-college.ac.jp>
2015-06-18 13:53:40 +0900

    List 17-1

---
+++ observer/src/jp/ac/it_college/std/s14006/observer/Observer.java
@@ -0,0 +1,9 @@
+package jp.ac.it_college.std.s14006.observer;
+
+/**
+ * Created by kabotya on 15/06/18.
+ */
+public interface Observer {
+
+    public abstract void update(NumberGenerator generator);
+}
