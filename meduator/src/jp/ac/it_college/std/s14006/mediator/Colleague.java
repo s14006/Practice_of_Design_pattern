4605e070191c3e9214fd55e92ca69f20b3957ede
s14006 <s14006@std.it-college.ac.jp>
2015-06-18 11:04:48 +0900

    List 16-2

---
+++ meduator/src/jp/ac/it_college/std/s14006/mediator/Colleague.java
@@ -0,0 +1,10 @@
+package jp.ac.it_college.std.s14006.mediator;
+
+/**
+ * Created by kabotya on 15/06/18.
+ */
+public interface Colleague {
+
+    public abstract void setMediator(Mediator mediator);
+    public abstract void setCollegueEnabled(boolean enabled)
+}
