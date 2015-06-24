db10066e0a93c7f59157635de6d2d61e2832c8f5
s14006 <s14006@std.it-college.ac.jp>
2015-06-24 13:56:10 +0900

    List 19-1

---
+++ state/src/jp/ac/it_college/std/s14006/state/State.java
@@ -0,0 +1,12 @@
+package jp.ac.it_college.std.s14006.state;
+
+/**
+ * Created by kabotya on 15/06/24.
+ */
+public interface State {
+
+    public abstract void doClock(Contect contect, int hour);
+    public abstract void doUse(Context context);
+    public abstract void doAlarm(Context context);
+    public abstract void doPhone(Context context);
+}
