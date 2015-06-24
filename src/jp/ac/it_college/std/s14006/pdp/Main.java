3f1aa3baa877e36b662ceda856852103f65c2218
s14006 <s14006@std.it-college.ac.jp>
2015-06-24 13:39:16 +0900

    List 14-Main

--- src/jp/ac/it_college/std/s14006/pdp/Main.java
+++ src/jp/ac/it_college/std/s14006/pdp/Main.java
@@ -58,22 +58,6 @@ public class Main {
         new ChainOfResponsibilityMain();
 
     }
-    public static void Chapter14() {
-
-        Support azrs = new NoSupport("azrs");
-        Support kabotya = new LimitSupport("kabotya", 100);
-        Support cloneko = new SpecialSupport("cloneko", 429);
-        Support ito = new LimitSupport("ito", 200);
-        Support usathon = new OddSupport("usathon");
-        Support usao = new LimitSupport("usao", 300);
-
-        azrs.setNext(kabotya).setNext(cloneko).setNext(ito).setNext(usathon).setNext(usao);
-
-        for (int i = 0; i < 500; i++) {
-            azrs.support(new Trouble(i));
-        }
-    }
-
     public static void Chapter15() {
 
         PageMaker.makeWelcomePage("kabotya@kabotya.com", "welcome.html");
