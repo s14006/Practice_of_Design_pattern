1af77d5335e6dc1bde94efcb0a4a14cdc0b9b619
s14006 <s14006@std.it-college.ac.jp>
2015-06-24 14:11:15 +0900

    List 19-3

---
+++ state/src/jp/ac/it_college/std/s14006/state/NightState.java
@@ -0,0 +1,38 @@
+package jp.ac.it_college.std.s14006.state;
+
+
+/**
+ * Created by kabotya on 15/06/24.
+ */
+public class NightState implements State{
+
+    private static NightState singleton = new NightState();
+
+    private NightState() {}
+
+    public static State getInstance() {
+        return singleton;
+    }
+
+    public void doClock(Context context, int hour) {
+        if (9 <= hour && hour < 17) {
+            context.changeState(DayState.getInstance());
+        }
+    }
+
+    public void doUse(Context context) {
+        context.callSecurityCenter("非常:夜間の金庫使用!");
+    }
+
+    public void doAlarm(Context context) {
+        context.callSecurityCenter("非常ベル(夜間)");
+    }
+
+    public void doPhone(Context context) {
+        context.callSecurityCenter("夜間の通話録音");
+    }
+
+    public String toString() {
+        return "[夜間]"
+    }
+}
