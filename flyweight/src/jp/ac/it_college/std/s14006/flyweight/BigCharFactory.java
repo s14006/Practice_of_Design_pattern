8715527c9e40351ba48559f3a000a050054b8f86
s14006 <s14006@std.it-college.ac.jp>
2015-06-24 20:11:36 +0900

    List 20-13

---
+++ flyweight/src/jp/ac/it_college/std/s14006/flyweight/BigCharFactory.java
@@ -0,0 +1,28 @@
+package jp.ac.it_college.std.s14006.flyweight;
+
+import java.util.HashMap;
+
+/**
+ * Created by kabotya on 15/06/24.
+ */
+public class BigCharFactory {
+
+    private HashMap pool = new HashMap();
+
+    private static BigCharFactory singleton = new BigCharFactory();
+
+    public BigCharFactory() {
+        return singleton;
+    }
+
+    public synchronized BigChar getBigChar(char charname) {
+        BigChar bc = (BigChar)pool.get("" + charname);
+        if (bc == null) {
+            bc = new BigChar(charname);
+            pool.put("" + charname, bc);
+        }
+        return bc;
+    }
+
+
+}
