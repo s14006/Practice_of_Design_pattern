3fb785d683c8901af7267bfebd042986297ffdfa
s14006 <s14006@std.it-college.ac.jp>
2015-06-24 14:04:29 +0900

    List 19-2

--- state/src/jp/ac/it_college/std/s14006/state/State.java
+++ state/src/jp/ac/it_college/std/s14006/state/State.java
@@ -5,7 +5,7 @@ package jp.ac.it_college.std.s14006.state;
  */
 public interface State {
 
-    public abstract void doClock(Contect contect, int hour);
+    public abstract void doClock(Context context, int hour);
     public abstract void doUse(Context context);
     public abstract void doAlarm(Context context);
     public abstract void doPhone(Context context);
