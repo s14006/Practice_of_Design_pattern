0bb99b34d51b346f792c5e944ecd42cd89c6e3b5
s14006 <s14006@std.it-college.ac.jp>
2015-06-24 13:24:34 +0900

    List 10-Main

--- src/jp/ac/it_college/std/s14006/pdp/Main.java
+++ src/jp/ac/it_college/std/s14006/pdp/Main.java
@@ -22,10 +22,7 @@ import jp.ac.it_college.std.s14006.memento.Gamer;
 import jp.ac.it_college.std.s14006.memento.Memento;
 import jp.ac.it_college.std.s14006.prototype.PrototypeMain;
 import jp.ac.it_college.std.s14006.singleton.SingletonMain;
-import jp.ac.it_college.std.s14006.strategy.Hand;
-import jp.ac.it_college.std.s14006.strategy.Player;
-import jp.ac.it_college.std.s14006.strategy.ProbStrategy;
-import jp.ac.it_college.std.s14006.strategy.WinningStrategy;
+import jp.ac.it_college.std.s14006.strategy.StrategyMain;
 import jp.ac.it_college.std.s14006.template_method.TemplateMethodMain;
 import std.Observer.*;
 import std.Observer.Observer;
@@ -52,50 +49,12 @@ public class Main {
 
         //new FactoryMain(new String[] {""});   Chapter8 factory
 
-        new BridgeMain();
+        //new BridgeMain();         Chapter9 bridge
+
+        new StrategyMain(new String[] {"auau"});
 
 
     }
-    public static void Chapter10(String[] args) {
-
-        if (args.length != 2) {
-            System.out.println("Usage: java Main randomseed1 randomseed2");
-            System.out.println("Example: java Main 314 15");
-            System.exit(0);
-        }
-
-        int seed1 = Integer.parseInt(args[0]);
-        int seed2 = Integer.parseInt(args[1]);
-
-        Player player1 = new Player("Taro", new WinningStrategy(seed1));
-        Player player2 = new Player("Hana", new ProbStrategy(seed2));
-
-        for (int i = 0; i < 10000; i++) {
-            Hand nextHand1 = player1.nextHand();
-            Hand nextHand2 = player2.nextHand();
-
-            if (nextHand1.isStrongerThan(nextHand2)) {
-                System.out.println("Winner: " + player1);
-                player1.win();
-                player2.lose();
-
-            } else if (nextHand2.isStrongerThan(nextHand1)) {
-                System.out.println("Winner: " + player2);
-                player1.lose();
-                player2.win();
-
-            } else {
-                System.out.println("Even…");
-                player1.even();
-                player2.even();
-            }
-        }
-
-        System.out.println("Total result:");
-        System.out.println(player1.toString());
-        System.out.println(player2.toString());
-    }
-
     public static void Chapter11() {
 
         try {
