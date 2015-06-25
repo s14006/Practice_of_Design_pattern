87144b8a52e534afd55002a3d54aebd44a8605b6
s14006 <s14006@std.it-college.ac.jp>
2015-06-25 10:12:19 +0900

    List 22-2

--- command/src/jp/ac/it_college/std/s14006/command/MacroCommand.java
+++ command/src/jp/ac/it_college/std/s14006/command/MacroCommand.java
@@ -16,4 +16,20 @@ public class MacroCommand implements Command {
             ((Command)it.next()).execute();
         }
     }
+
+    public void append(Command cmd) {
+        if (cmd != this) {
+            commands.push(cmd);
+        }
+    }
+
+    public void undo() {
+        if (!commands.empty()) {
+            commands.pop();
+        }
+    }
+
+    public void clear() {
+        commands.clear();
+    }
 }
